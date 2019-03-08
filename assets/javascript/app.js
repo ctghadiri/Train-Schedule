0e3ec866284b4305652c3b1771ab9fd17b5f005f
ctghadiri <ctghadiri@gmail.com>
2019-03-07 16:02:32 -0800

    add assets folder + pseudocode js + add links

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+// Train Game
+// area for train information
+    // name
+    // destination
+    // frequency(min)
+    // next arrival (a.m./p.m.)
+    // min away
+// form for inputing new train data
+    // name
+    // destination
+    // first train time
+    // frequency (min)
+    // submit button
+// firebase to hold data
+    // 
+
+
+
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB9zfOvWjhA-2ItOOk9HBTIpPaSbN8U184",
+    authDomain: "train-schedule-4005d.firebaseapp.com",
+    databaseURL: "https://train-schedule-4005d.firebaseio.com",
+    projectId: "train-schedule-4005d",
+    storageBucket: "train-schedule-4005d.appspot.com",
+    messagingSenderId: "557889070241"
+  };
+  firebase.initializeApp(config);
