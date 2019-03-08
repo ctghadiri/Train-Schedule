8565cdead06b55a259a8d5bb58e71b3367e0764f
ctghadiri <ctghadiri@gmail.com>
2019-03-07 19:54:14 -0800

    adjest ids and add variable + links to values

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,12 +19,32 @@
 
 
   // Initialize Firebase
-  var config = {
+var config = {
     apiKey: "AIzaSyB9zfOvWjhA-2ItOOk9HBTIpPaSbN8U184",
     authDomain: "train-schedule-4005d.firebaseapp.com",
     databaseURL: "https://train-schedule-4005d.firebaseio.com",
     projectId: "train-schedule-4005d",
     storageBucket: "train-schedule-4005d.appspot.com",
     messagingSenderId: "557889070241"
-  };
-  firebase.initializeApp(config);
+};
+
+firebase.initializeApp(config);
+
+var dataRef = firebase.database();
+
+// initial values
+var trainName = "";
+var destination = "";
+var initialTime = "";
+var frequency = "";
+
+// capture button click
+$("#add-train").on("click", function(event){
+    event.preventDefault();
+
+    trainName = $("#train-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    initialTime = $("#initial-time-input").val().trim();
+    frequency = $("#frequency-input").val().trim();
+})
+
